baafe09bbe9dfdde1498f31fc0d837bfd88828c6
Jacopo Donati <33698919+cantaprete@users.noreply.github.com>
2026-05-13 09:32:10 +0200

    fix manual plot

--- docs/manual_print.sh
+++ docs/manual_print.sh
@@ -7,9 +7,8 @@ python3 print_comparison_plots.py ../data/meaningful-sample.csv meaningful
 python3 print_plots.py ../data/control-sample.csv random_subset $(wc -l ../data/meaningful-sample.csv | awk '{print $1;}')
 python3 print_comparison_plots.py ../data/control-sample.csv random_subset $(wc -l ../data/meaningful-sample.csv | awk '{print $1;}')
 
-magick montage meaningful_bars.png random_subset_bars.png -geometry +1+1 comparison_bars.png
-magick montage meaningful_moving_no.png random_subset_moving_no.png -geometry +1+1 comparison_moving_no.png
-magick montage meaningful_moving.png random_subset_moving.png -geometry +1+1 comparison_moving.png
-magick montage meaningful_radar.png random_subset_radar.png -geometry +1+1 comparison_radar.png
-magick montage meaningful_pie.png random_subset_pie.png -geometry +1+1 comparison_pie.png
+montage meaningful_bars.png random_subset_bars.png -geometry +1+1 comparison_bars.png
+montage meaningful_moving_no.png random_subset_moving_no.png -geometry +1+1 comparison_moving_no.montage meaningful_moving.png random_subset_moving.png -geometry +1+1 comparison_moving.png
+montage meaningful_radar.png random_subset_radar.png -geometry +1+1 comparison_radar.png
+montage meaningful_pie.png random_subset_pie.png -geometry +1+1 comparison_pie.png
 montage meaningful_heatmap.png random_subset_heatmap.png -geometry +1+1 comparison_heatmap.png
