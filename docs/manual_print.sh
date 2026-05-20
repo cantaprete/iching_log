13332a27a8c3d649682495a87b509f51a42c4559
Jacopo Donati <33698919+cantaprete@users.noreply.github.com>
2026-05-20 11:39:48 +0200

    aggiornamento manual-print

--- docs/manual_print.sh
+++ docs/manual_print.sh
@@ -3,14 +3,13 @@
 [ ! -d "./docs" ] && mkdir docs
 
 python3 print_plots.py ../data/control-sample.csv random
-python3 print_comparison_plots.py ../data/control-sample.csv random
 python3 print_plots.py ../data/meaningful-sample.csv meaningful
-python3 print_comparison_plots.py ../data/meaningful-sample.csv meaningful
 python3 print_plots.py ../data/control-sample.csv random_subset $(wc -l ../data/meaningful-sample.csv | awk '{print $1;}')
-python3 print_comparison_plots.py ../data/control-sample.csv random_subset $(wc -l ../data/meaningful-sample.csv | awk '{print $1;}')
+python3 print_comparison_plots.py ../data/meaningful-sample.csv meaningful ../data/control-sample.csv random_subset $(wc -l ../data/meaningful-sample.csv | awk '{print $1;}')
 
-montage meaningful_bars.png random_subset_bars.png -geometry +1+1 comparison_bars.png
-montage meaningful_moving_no.png random_subset_moving_no.png -geometry +1+1 comparison_moving_no.montage meaningful_moving.png random_subset_moving.png -geometry +1+1 comparison_moving.png
-montage meaningful_radar.png random_subset_radar.png -geometry +1+1 comparison_radar.png
-montage meaningful_pie.png random_subset_pie.png -geometry +1+1 comparison_pie.png
-montage meaningful_heatmap.png random_subset_heatmap.png -geometry +1+1 comparison_heatmap.png
+montage docs/meaningful_bars.png docs/random_subset_bars.png -geometry +1+1 docs/comparison_bars.png
+montage docs/meaningful_moving_no.png docs/random_subset_moving_no.png -geometry +1+1 docs/comparison_moving_no.png
+montage docs/meaningful_moving.png docs/random_subset_moving.png -geometry +1+1 docs/comparison_moving.png
+montage docs/meaningful_radar.png docs/random_subset_radar.png -geometry +1+1 docs/comparison_radar.png
+montage docs/meaningful_pie.png docs/random_subset_pie.png -geometry +1+1 docs/comparison_pie.png
+montage docs/meaningful_heatmap.png docs/random_subset_heatmap.png -geometry +1+1 docs/comparison_heatmap.png
